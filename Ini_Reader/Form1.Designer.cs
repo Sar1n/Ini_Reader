890ee887fbe88432eea3f6cf316922b14d0d1cc4
Ivan Maluk <ivan.maluk@gmail.com>
2019-03-09 19:54:53 +0200

    Solution

---
+++ Ini_Reader/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Ini_Reader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(67, 20);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Ini Path:";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox1.Location = new System.Drawing.Point(85, 6);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(457, 26);
+			this.textBox1.TabIndex = 1;
+			// 
+			// button1
+			// 
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button1.Location = new System.Drawing.Point(548, 6);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(70, 27);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "Open";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// panel1
+			// 
+			this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.panel1.AutoScroll = true;
+			this.panel1.BackColor = System.Drawing.SystemColors.Control;
+			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel1.Location = new System.Drawing.Point(12, 39);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(682, 475);
+			this.panel1.TabIndex = 4;
+			// 
+			// button2
+			// 
+			this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button2.Location = new System.Drawing.Point(624, 6);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(70, 27);
+			this.button2.TabIndex = 5;
+			this.button2.Text = "Browse";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(706, 526);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.MinimumSize = new System.Drawing.Size(400, 200);
+			this.Name = "Form1";
+			this.Text = "Ini Reader";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button button2;
+	}
+}
+
---
+++ Ini_Reader/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ini_Reader
+{
+    public partial class Form1 : Form
+    {
+		int flowHeight = 5, flowWidth = 5; //позиционирование надписей на форме
+		string prev = ""; //предыдущее значение ключа
+
+		public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) //открытие файла по пути
+        {
+		   if (textBox1.Text != "")
+			{
+				panel1.Controls.Clear();
+				flowWidth = 5;
+				Display(textBox1.Text);
+			}
+		   else MessageBox.Show("Please, insert the path");
+		}
+
+		private void button2_Click(object sender, EventArgs e) //открытие файла дилоговым окном
+		{
+			try
+			{ 
+				var fileDialog = new OpenFileDialog();
+				if (fileDialog.ShowDialog() == DialogResult.OK)
+				{
+					panel1.Controls.Clear();
+					flowWidth = 5;
+					Display(fileDialog.FileName);
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.ToString());
+			}
+		}
+
+		void CreateSection(string section) //вывод секции на форму
+		{
+			flowHeight = 5;
+			Label sectionlabel = new Label();
+			sectionlabel.Text = "[" + section + "]";
+			sectionlabel.Location = new Point(flowHeight, flowWidth);
+			panel1.Controls.Add(sectionlabel);
+			flowWidth += 30;
+		}
+		void CreateNameValue(string key, string value) //вывод пары ключ - значение
+		{
+			flowHeight += 20; //отступ
+			TextBox valueblock = new TextBox();
+			if (key == prev) //если у данного значения ключ как у предыдущего
+			{
+				flowWidth -= 30;
+				flowHeight += 130;
+
+				valueblock.Text = value;
+				valueblock.Size = new Size(120, 25);
+				valueblock.Location = new Point(flowHeight, flowWidth);
+				panel1.Controls.Add(valueblock);
+			}
+			else
+			{
+				prev = key;
+
+				flowHeight = 5;					//вывод ключа 
+				TextBox keyblock = new TextBox();
+				keyblock.Text = key;
+				keyblock.Size = new Size(120, 25);
+				keyblock.Location = new Point(flowHeight, flowWidth);
+				panel1.Controls.Add(keyblock);
+				flowHeight += 130;
+
+				Label eq = new Label();			//вывод =
+				eq.Text = "=";
+				eq.Size = new Size(15, 13);
+				eq.Location = new Point(flowHeight, flowWidth);
+				panel1.Controls.Add(eq);
+				flowHeight += 25;
+
+				valueblock.Text = value;		//вывод значения
+				valueblock.Size = new Size(120, 25);
+				valueblock.Location = new Point(flowHeight, flowWidth);
+				panel1.Controls.Add(valueblock);
+			}
+			flowWidth += 30;
+		}
+
+		string TrimComment(string s) //вырезаем комментарий из строки
+		{
+			if (s.Contains(";"))
+			{
+				int index = s.IndexOf(';');
+				s = s.Substring(0, index).Trim();
+			}
+			return s;
+		}
+
+		private void Display(string path) //метод чтения из файла
+		{
+			try
+			{
+				StreamReader reader = new StreamReader(path);
+				string line;
+
+				while ((line = reader.ReadLine()) != null)
+				{
+					line = TrimComment(line);
+
+					if (line.Length == 0) continue;
+
+					if (line.StartsWith("[") && line.Contains("]"))  //section
+					{
+						int index = line.IndexOf(']');
+						string sectionName = line.Substring(1, index - 1).Trim();
+						CreateSection(sectionName);
+						continue;
+					}
+
+					if (line.Contains("="))  //key = value
+					{
+						int index = line.IndexOf('=');
+						string key = line.Substring(0, index).Trim();
+						string value = "";
+						if (!line.Contains(",")) //если значиение одно
+						{
+							value = line.Substring(index + 1).Trim();
+							CreateNameValue(key, value);
+						}
+						else //если значений несколько
+						{
+							line = line.Substring(index + 1).Trim();
+							int indexcomma = 0;
+							while (line.Contains(","))
+							{
+								indexcomma = line.IndexOf(",");
+								value = line.Substring(0, indexcomma).Trim();
+								line = line.Substring(indexcomma + 1).Trim();
+								CreateNameValue(key, value);
+							}
+							value = line.Trim();
+							CreateNameValue(key, value);
+						}
+					}
+				}
+				reader.Close();
+			}
+			catch (ArgumentException)
+			{
+				MessageBox.Show("Please, insert correct path");
+			}
+			catch (FileNotFoundException)
+			{
+				MessageBox.Show("File not found");
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.ToString());
+			}
+		}
+
+	}
+}
